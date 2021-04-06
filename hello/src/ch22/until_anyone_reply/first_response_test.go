d7940dd056ead46abd7f6da0580fe647bfef71d8
guoshanyi <1024743675@qq.com>
2021-04-06 23:02:58 +0800

    对象池

---
+++ hello/src/ch22/until_anyone_reply/first_response_test.go
@@ -0,0 +1,47 @@
+package until_anyone_reply
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func runTask(id int) string {
+	time.Sleep(10 * time.Millisecond)
+	return fmt.Sprintf("the result is from %d", id)
+}
+func firstResponse() string {
+	numOfRunner := 10
+	ch := make(chan string, numOfRunner)
+	for i := 0; i < numOfRunner; i++ {
+		go func(i int) {
+			ret := runTask(i)
+			ch <- ret
+		}(i)
+	}
+	return <-ch
+}
+
+func TestFirstResponse(t *testing.T) {
+	t.Log("Before:", runtime.NumGoroutine())
+	t.Log(allResponse())
+	time.Sleep(time.Second * 1)
+	t.Log("After:", runtime.NumGoroutine())
+}
+
+func allResponse() string {
+	numOfRunner := 10
+	ch := make(chan string, numOfRunner)
+	for i := 0; i < numOfRunner; i++ {
+		go func(i int) {
+			ret := runTask(i)
+			ch <- ret
+		}(i)
+	}
+	finalRet := ""
+	for i := 0; i < numOfRunner; i++ {
+		finalRet += <-ch + "\n"
+	}
+	return finalRet
+}
---
+++ hello/src/ch32/obj_pool/obj_pool_test.go
@@ -0,0 +1,59 @@
+package obj_pool
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+	"time"
+)
+
+type ReusableObj struct {
+}
+type ObjPool struct {
+	bufChan chan *ReusableObj //用于缓冲可重用对象
+}
+
+func newObjPool(numOfObj int) *ObjPool {
+	objPool := ObjPool{}
+	objPool.bufChan = make(chan *ReusableObj, numOfObj)
+	for i := 0; i < numOfObj; i++ {
+		objPool.bufChan <- &ReusableObj{}
+	}
+	return &objPool
+}
+
+func (p *ObjPool) GetObj(timeout time.Duration) (*ReusableObj, error) {
+	select {
+	case ret := <-p.bufChan:
+		return ret, nil
+	case <-time.After(timeout): //超时控制
+		return nil, errors.New("time out")
+	default:
+		return nil, errors.New("overflow")
+	}
+
+}
+
+func (p *ObjPool) ReleaseObj(obj *ReusableObj) error {
+	select {
+	case p.bufChan <- obj:
+		return nil
+	default:
+		return errors.New("overflow")
+	}
+}
+
+func TestObjPool(t *testing.T) {
+	pool := newObjPool(10)
+	for i := 0; i < 11; i++ {
+		if v, err := pool.GetObj(time.Second * 1); err != nil {
+			t.Error(err)
+		} else {
+			fmt.Println("%T\n", v)
+			if err := pool.ReleaseObj(v); err != nil {
+				t.Error(err)
+			}
+		}
+	}
+	fmt.Println("Done")
+}
