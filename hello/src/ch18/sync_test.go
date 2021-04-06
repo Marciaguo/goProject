bfc0154ba5229d0e82e05234a51b36d89aefea4b
guoshanyi <1024743675@qq.com>
2021-04-06 18:55:41 +0800

    多路选择

---
+++ hello/src/ch18/sync_test.go
@@ -0,0 +1,39 @@
+package ch18
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func service() string {
+	time.Sleep(time.Microsecond * 50)
+	return "Done"
+}
+func otherTask() {
+	fmt.Println("working on something else")
+	time.Sleep(time.Microsecond * 100)
+	fmt.Println("Task is done.")
+}
+func TestService(t *testing.T) {
+	fmt.Println(service())
+	otherTask()
+}
+
+func AsyncService() chan string {
+	retCh := make(chan string)
+	go func() {
+		ret := service()
+		fmt.Println("returned result")
+		retCh <- ret
+		fmt.Println("service exited")
+	}()
+	return retCh
+}
+
+func TestAsyncService(t *testing.T) {
+	retCh := AsyncService()
+	otherTask()
+	fmt.Println(<-retCh)
+	time.Sleep(time.Second * 1)
+}
